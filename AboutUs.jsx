6f8dc5b6a37c51128e5919a04c5655cdb5761cb3
bachkhai18-alt <bachkhai18@gmail.com>
2026-05-06 17:48:03 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <p className="about-us-description">
+        At Paradise Nursery, we believe that every home deserves a touch of nature. 
+        Our mission is to provide high-quality, vibrant houseplants that not only 
+        beautify your space but also improve your well-being. From air-purifying 
+        ferns to easy-to-care-for succulents, we have something for every plant lover.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
